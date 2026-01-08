1569973d70efc2d552bf72ab361db964ce58c6ab
Alberto <campsbass@gmail.com>
2026-01-08 12:03:43 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const webPageInfo = {
+    title: "Mi primera página con Node.js loco!",
+    subtitle: "Aprendiendo a crear un servidor web",
+    description: "Que pasa wacho, esta es mi primera página creada con Node.js"
+}
+
+module.exports = webPageInfo;
